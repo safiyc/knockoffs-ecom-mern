a8b462c12f7a204b433b0308a0b8aa0105c39995
Safiy Cham <csafiy@yahoo.com>
2020-11-04 09:00:36 -0800

    React router to product page

--- frontend/frontend-react/src/App.js
+++ frontend/frontend-react/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+// import data from './dummyData';
 
 function App() {
   const openMenu = () => {
@@ -10,103 +15,44 @@ function App() {
   }
 
   return (
-    <div className='grid-container'>
-      <header className='header'>
-        <div className='brand'>
-          <button onClick={openMenu}>&#9776;</button>
-          <a href='index.html'>Knockoffs</a>
-        </div>
-        <div className='header-links'>
-          <a href='cart.html'>Cart</a>
-          <a href='signin.html'>Sign In</a>
-        </div>
-      </header>
-      <aside className='sidebar'>
-        <h3>Categories</h3>
-        <button className='sidebar-close-button' onClick={closeMenu}>X</button>
-        <ul>
-          <li>
-            <a href='index.html'>Food</a>
-          </li>
-          <li>
-            <a href='index.html'>Clothes</a>
-          </li>
-          <li>
-            <a href='index.html'>Electronics</a>
-          </li>
-        </ul>
-      </aside>
-      <main className='main'>
-        <ul className='products'>
-          <li>
-            <div className='product'>
-              <img className='product-image' src='/images/iTap.webp' alt='product' />
-              <div className='product-name'>iTap</div>
-              <div className='product-brand'>Apricot</div>
-              <div className='product-price'>$549</div>
-              <div className='product-rating'>3.5 Stars (15 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className='product'>
-              <img className='product-image' src='/images/iTap.webp' alt='product' />
-              <div className='product-name'>iTap</div>
-              <div className='product-brand'>Apricot</div>
-              <div className='product-price'>$549</div>
-              <div className='product-rating'>3.5 Stars (15 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className='product'>
-              <img className='product-image' src='/images/iTap.webp' alt='product' />
-              <div className='product-name'>iTap</div>
-              <div className='product-brand'>Apricot</div>
-              <div className='product-price'>$549</div>
-              <div className='product-rating'>3.5 Stars (15 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className='product'>
-              <img className='product-image' src='/images/iTap.webp' alt='product' />
-              <div className='product-name'>iTap</div>
-              <div className='product-brand'>Apricot</div>
-              <div className='product-price'>$549</div>
-              <div className='product-rating'>3.5 Stars (15 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className='product'>
-              <img className='product-image' src='/images/iTap.webp' alt='product' />
-              <div className='product-name'>iTap</div>
-              <div className='product-brand'>Apricot</div>
-              <div className='product-price'>$549</div>
-              <div className='product-rating'>3.5 Stars (15 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className='product'>
-              <img className='product-image' src='/images/iTap.webp' alt='product' />
-              <div className='product-name'>iTap</div>
-              <div className='product-brand'>Apricot</div>
-              <div className='product-price'>$549</div>
-              <div className='product-rating'>3.5 Stars (15 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className='product'>
-              <img className='product-image' src='/images/iTap.webp' alt='product' />
-              <div className='product-name'>iTap</div>
-              <div className='product-brand'>Apricot</div>
-              <div className='product-price'>$549</div>
-              <div className='product-rating'>3.5 Stars (15 reviews)</div>
-            </div>
-          </li>
-        </ul>
-      </main>
-      <footer className='footer'>
-        All right reserved.
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header className='header'>
+          <div className='brand'>
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to='/'>Knockoffs</Link>
+          </div>
+          <div className='header-links'>
+            <a href='cart.html'>Cart</a>
+            <a href='signin.html'>Sign In</a>
+          </div>
+        </header>
+        <aside className='sidebar'>
+          <h3>Categories</h3>
+          <button className='sidebar-close-button' onClick={closeMenu}>X</button>
+          <ul>
+            <li>
+              <a href='index.html'>Food</a>
+            </li>
+            <li>
+              <a href='index.html'>Clothes</a>
+            </li>
+            <li>
+              <a href='index.html'>Electronics</a>
+            </li>
+          </ul>
+        </aside>
+        <main className='main'>
+          <div className='content'>
+            <Route path='product/:id' component={ProductScreen} />
+            <Route path='/' exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className='footer'>
+          All right reserved.
       </footer>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/frontend-react/src/dummyData.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'iTap',
+      brand: 'Apricot',
+      category: 'electronics',
+      image: '/images/iTap.webp',
+      price: 250,
+      rating: 3.5,
+      numReviews: 25
+    },
+    {
+      _id: '2',
+      name: 'iTap2',
+      brand: 'Apricot',
+      category: 'electronics',
+      image: '/images/iTap.webp',
+      price: 450,
+      rating: 4.5,
+      numReviews: 105
+    },
+    {
+      _id: '3',
+      name: 'Frosted Fakes',
+      brand: 'Miller',
+      category: 'food',
+      image: '/images/iTap.webp',
+      price: 10.25,
+      rating: 4.8,
+      numReviews: 25
+    },
+    {
+      _id: '4',
+      name: 'Slim Polo',
+      brand: 'H&N',
+      category: 'clothes',
+      image: '/images/iTap.webp',
+      price: 24,
+      rating: 5,
+      numReviews: 35
+    }
+  ]
+}
+
+export default data;
