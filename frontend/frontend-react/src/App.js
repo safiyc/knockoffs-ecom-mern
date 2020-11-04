4f45120ae6dc8dc88cf6b2491cff8cfb7b7a3ff9
Safiy Cham <csafiy@yahoo.com>
2020-11-04 10:37:02 -0800

    Add info to Product page

--- frontend/frontend-react/src/App.js
+++ frontend/frontend-react/src/App.js
@@ -44,7 +44,7 @@ function App() {
         </aside>
         <main className='main'>
           <div className='content'>
-            <Route path='product/:id' component={ProductScreen} />
+            <Route path='/product/:id' component={ProductScreen} />
             <Route path='/' exact={true} component={HomeScreen} />
           </div>
         </main>
--- frontend/frontend-react/src/Screens/HomeScreen.js
+++ frontend/frontend-react/src/Screens/HomeScreen.js
@@ -7,7 +7,7 @@ function HomeScreen(props) {
     <ul className='products'>
       {
         data.products.map(product =>
-          <li>
+          <li key={product._id}>
             <div className='product'>
               <Link to={'/product/' + product._id}>
                 <img className='product-image' src={product.image} alt='product' />
